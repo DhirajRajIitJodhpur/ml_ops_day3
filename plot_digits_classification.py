d6c9d9dfabeeaf778b3cf27b3e48b49f2e4f9430
Dhiraj Raj <dhirajrajiitj@Dhirajs-MacBook-Air.local>
2025-09-14 01:45:03 +0530

    created .gitignore

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -60,7 +60,7 @@ n_samples = len(digits.images)
 data = digits.images.reshape((n_samples, -1))
 
 # Create a classifier: a support vector classifier
-clf = svm.SVC(gamma=0.001)
+svm_clf = svm.SVC(gamma=0.001)
 
 # Split data into 50% train and 50% test subsets
 X_train, X_test, y_train, y_test = train_test_split(
@@ -68,10 +68,10 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Learn the digits on the train subset
-clf.fit(X_train, y_train)
+svm_clf.fit(X_train, y_train)
 
 # Predict the value of the digit on the test subset
-predicted = clf.predict(X_test)
+predicted = svm_clf.predict(X_test)
 
 ###############################################################################
 # Below we visualize the first 4 test samples and show their predicted
@@ -126,3 +126,11 @@ print(
     "Classification report rebuilt from confusion matrix:\n"
     f"{metrics.classification_report(y_true, y_pred)}\n"
 )
+
+
+########################################################################
+from joblib import dump, load
+best_model_path = './best_model.joblib'
+dump(clf,best_model_path)
+best_model = load(best_model_path)
+print("best_model_loaded")
