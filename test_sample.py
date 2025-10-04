1b3335a274322c0860ba389cbe578d519fcc41ce
Dhiraj Raj <dhirajrajiitj@Dhirajs-MacBook-Air.local>
2025-10-04 23:00:42 +0530

    pytest demo

--- test_sample.py
+++ test_sample.py
@@ -1,7 +1,26 @@
-from sklearn import datasets 
-
+from sklearn import datasets
+#from utils import read_digits
 def inc(x):
     return x+1 
 
-def test_anser():
-    assert inc(3)==5
+def test_answer():
+    assert inc(4)==5
+
+
+def answer_test():
+    assert inc(4)==5
+
+
+def test_answer():
+    assert inc(4)==5
+
+class Test1:
+    def _init_(self):
+        df=0 
+    def test_something(self):
+        assert 2==2
+
+
+class Test2:
+    def test_something(self):
+        assert 2==2
